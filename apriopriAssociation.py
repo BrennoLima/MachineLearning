a6d45fd2eac1d5ab8670a88479103432a908bec4
BrennoLima <52263373+BrennoLima@users.noreply.github.com>
2019-10-02 15:02:11 -0400

    Add files via upload

--- apriopriAssociation.py
+++ apriopriAssociation.py
@@ -27,7 +27,46 @@ from apyori import apriori
 rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
 
 # Visualizing
-results_list = []
-results = list(rules)
-for i in range(0, len(results)):
-    results_list.append(str(results[i][2]))
+
+
+# Arguments: rules from apriori method, number of associations
+def visualize(rules, n):
+    results_list = []
+    results = list(rules)
+    for i in range(0, len(results)):
+        results_list.append(str(results[i][2]))
+    
+    if len(results_list) < n:
+        n = len(results_list)
+    products = ''  
+    for i in range(0, n):
+            for j in range(0, len(results_list[i])):
+                if results_list[i][j] == '{':
+                    while True:
+                        j += 1
+                        if results_list[i][j] == '}':
+                            products += ' '
+                            break
+                        if results_list[i][j] == ',': 
+                            j+=1
+                        products += results_list[i][j]
+            products += ': ' + results_list[i][results_list[i].find('confidence')+13:results_list[i].find('confidence')+15] + '%\n'
+    products = products.replace("'nan'", '')  
+    return products
+
+print(visualize(rules, 10))
+    
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
